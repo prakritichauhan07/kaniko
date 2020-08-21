55e1a2bade97f46df132370039a1c6a806d89e28
prakritichauhan07 <67620689+prakritichauhan07@users.noreply.github.com>
2020-08-21 18:05:55 +0530

    Update travis-setup.sh

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -15,12 +15,22 @@
 
 set -ex
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
-curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-amd64 && chmod +x container-diff-linux-amd64 && sudo mv container-diff-linux-amd64 /usr/local/bin/container-diff
-docker run -d -p 5000:5000 --restart always --name registry registry:2
+if [ "$(uname -m)" == "aarch64" ]
+then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+  curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-arm64 && chmod +x container-diff-linux-arm64 && sudo mv container-diff-linux-arm64 /usr/local/bin/container-diff
+  docker run -d -p 5000:5000 --restart always --name registry registry:2
+else
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+  curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-amd64 && chmod +x container-diff-linux-amd64 && sudo mv container-diff-linux-amd64 /usr/local/bin/container-diff
+  docker run -d -p 5000:5000 --restart always --name registry registry:2
+fi
 
 mkdir -p $HOME/.docker/
 echo '{}' > $HOME/.docker/config.json
